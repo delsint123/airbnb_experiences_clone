ace28e2fc29c50fd8dd53666c26e25e4072bd221
delsint123 <delsint123@gmail.com>
2023-05-12 03:06:04 -0400

    Update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,13 @@ import photo_grid from "../assets/photo-grid.png"
 
 export default function Hero() {
     return (
-        <div className="hero">
+        <section className="hero">
             <img className="hero--photo_grid" src={photo_grid} />
 
             <h1 className="hero--title">Online Experiences</h1>
-            <h3 className="hero--subtitle">Join unique interactive activities led by one-of-a-kind 
+            <p className="hero--subtitle">Join unique interactive activities led by one-of-a-kind 
                 hosts-all without leaving home.
-            </h3>
-        </div>
+            </p>
+        </section>
     )
 }
