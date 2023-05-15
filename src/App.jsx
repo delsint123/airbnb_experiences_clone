b8ed72d6a6ac50f807169c85886d8876408aedc7
delsint123 <delsint123@gmail.com>
2023-05-15 16:37:55 -0400

    Add badges to cards

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ export default function App() {
                     location={card.location}
                     title={card.title}
                     price={card.price}
+                    openSpots={card.openSpots}
                 />
     })
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
 export default function Card(props) {
+
+    let badgeText
+    if(props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+
             <img className="card--image" src={props.img} />
             
             <div className="card--content">
